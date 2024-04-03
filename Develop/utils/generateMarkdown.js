3b5ce8218a500f6ebf3b1808f20bd19a8d4ac965
awatson622 <147440197+awatson622@users.noreply.github.com>
2024-04-02 18:06:57 -0700

    Update generateMarkdown.js
    updated code

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,40 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function to render license badge
+function renderLicenseBadge(license) {
+  if (license) {
+    // Return the appropriate license badge based on the license type
+    // Example: return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  } else {
+    // Return an empty string if there is no license
+    return '';
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function to render license link
+function renderLicenseLink(license) {
+  if (license) {
+    // Return the appropriate license link based on the license type
+    // Example: return `[License](https://opensource.org/licenses/MIT)`
+  } else {
+    // Return an empty string if there is no license
+    return '';
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function to render license section of README
+function renderLicenseSection(license) {
+  if (license) {
+    // Return the license section of README with appropriate formatting
+    // Example: return `## License
+    // This project is licensed under the MIT License
+  } else {
+    // Return an empty string if there is no license
+    return '';
+  }
+}
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return;
 }
 
 module.exports = generateMarkdown;
