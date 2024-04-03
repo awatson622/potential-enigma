e8250c26de8bd3b9d94bb26a14e49ff4e7cb4868
awatson622 <147440197+awatson622@users.noreply.github.com>
2024-04-02 18:06:29 -0700

    Update index.js
    updated code

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,40 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a brief description of your project:'
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log('README.md successfully generated!')
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions)
+    .then((data) => {
+      const markdown = generateMarkdown(data);
+      writeToFile('README.md', markdown);
+    })
+    .catch((error) => {
+      console.error('An error occurred:', error);
+    });
+}
 
 // Function call to initialize app
 init();
